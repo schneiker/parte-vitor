e40267c25872ea5e5b6f2f54b8623fec14daa9c6
KauanBatistaDosSantos <kauan18br@gmail.com>
2024-10-27 22:49:17 -0300

    ajustes do ts do cpf do entregador

--- src/app/components/painel-entregador/painel-entregador.component.ts
+++ src/app/components/painel-entregador/painel-entregador.component.ts
@@ -18,22 +18,47 @@ export class PainelEntregadorComponent {
   constructor(private entregadorService: EntregadorService, private router: Router) {}
 
   verificarCpf() {
-    const cpfSemMascara = this.removerMascaraCpf(this.cpf); // Remove máscara do CPF
+    const cpfSemMascara = this.removerMascaraCpf(this.cpf);
+
+    if (!this.validarCpf(cpfSemMascara)) {
+      this.erro = "CPF inválido. Verifique e tente novamente.";
+      return;
+    }
 
     this.entregadorService.getEntregadoresDisponiveis().subscribe(entregadores => {
       const entregador = entregadores.find(e => e.cpf.replace(/\D/g, '') === cpfSemMascara);
 
       if (entregador) {
-        // Formata o nome para a URL e redireciona para o perfil do entregador
         const nomeFormatado = entregador.nome.toLowerCase().replace(/\s+/g, '-');
         this.router.navigate([`/tela-inicial-entregador/${nomeFormatado}`]);
+        this.erro = null; // Limpa a mensagem de erro ao redirecionar
       } else {
         this.erro = "CPF não encontrado. Verifique e tente novamente.";
       }
     });
   }
 
-  private removerMascaraCpf(cpf: string): string {
-    return cpf.replace(/\D/g, ''); // Remove tudo que não é dígito
+  formatarCpf() {
+    let cpfNumeros = this.removerMascaraCpf(this.cpf);
+    if (cpfNumeros.length > 11) {
+      cpfNumeros = cpfNumeros.substring(0, 11);
+    }
+    
+    this.cpf = cpfNumeros.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    
+    // Exibe a mensagem de erro apenas quando o CPF tiver 11 dígitos e for inválido
+    if (cpfNumeros.length === 11 && !this.validarCpf(cpfNumeros)) {
+      this.erro = "CPF inválido. Certifique-se de que não é uma sequência repetitiva e possui 11 dígitos.";
+    } else {
+      this.erro = null; // Limpa a mensagem de erro se o CPF se tornar válido ou incompleto
+    }
+  }
+
+  public removerMascaraCpf(cpf: string): string {
+    return cpf.replace(/\D/g, '');
+  }
+
+  public validarCpf(cpf: string): boolean {
+    return cpf.length === 11 && !/^(\d)\1+$/.test(cpf); // Verifica 11 dígitos e evita sequência repetitiva
   }
 }
