48fba83cc253cd454138af36cfc54603e8bdcff4
schneiker <vitorschneiker@gmail.com>
2024-10-29 16:16:54 -0300

    Adicionei dish form e dish list, telas criadas pela sabryna

--- src/app/components/dish-list/dish-list.component.ts
+++ src/app/components/dish-list/dish-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DishService, Dish } from '../../services/dish.service';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router'; 
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dish-list',
@@ -13,7 +14,7 @@ import { Router, RouterModule } from '@angular/router';
 export class DishListComponent implements OnInit {
   dishes: Dish[] = [];
 
-  constructor(private dishService: DishService, private router: Router) {}
+  constructor(private dishService: DishService, private router: Router, private location:Location) {}
 
   ngOnInit(): void {
     this.loadDishes();
@@ -33,5 +34,9 @@ export class DishListComponent implements OnInit {
 
   editDish(id: string) {
     this.router.navigate(['/dish-form', id]);
+  }
+
+  voltar() {
+    this.location.back()
   }
 }
